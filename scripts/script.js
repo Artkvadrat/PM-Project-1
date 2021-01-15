3d563cd3f79076cb9e4052c4232a00ae9a53170b
Danylo.Nabok <nabok.danylo@gmail.com>
2021-01-15 16:15:44 +0200

    Added script for top-menu navigation that takes data from config file.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,26 @@
-console.log(CURRENCY);
+let topMenuContainer = document.getElementsByClassName('navigation')[0];
+
+let dataForTopMenu = Object.values(TOP_MENU).sort((a, b) => {
+    return a.order - b.order;
+});
+
+let topMenuFilling = '<ul>';
+let amountOfMenuItems = Math.min(9, dataForTopMenu.length);
+for (let i = 0; i < amountOfMenuItems; i++) {
+    let result = '<li>';
+    if (dataForTopMenu[i].submenu) {
+        result += `<p class="dropdownMenu">${dataForTopMenu[i].title}</p><div>`;
+        dataForTopMenu[i].submenu.map((subMenuItem) => {
+            result += `<a href="${subMenuItem.url}">${subMenuItem.title}</a>`;
+        });
+        result += '</div>'
+    } else {
+        result += `<a href="${dataForTopMenu[i].url}">${dataForTopMenu[i].title}</a>`
+    }
+    result += '</li>';
+    topMenuFilling += result;
+}
+topMenuFilling += '</ul>';
+
+topMenuContainer.innerHTML = topMenuFilling;
+
