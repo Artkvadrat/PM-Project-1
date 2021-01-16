c4c184db080f1bf852bb88fa1fbbf765f9c8184f
Danylo.Nabok <nabok.danylo@gmail.com>
2021-01-16 13:50:50 +0200

    Added script for catalog menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,141 @@
 /*
  * Data for top menu from config file
  */
-let topMenuContainer = document.getElementsByClassName('navigation')[0];
+try {
+    if (Object.keys(TOP_MENU).length !== 0) {
+        let topMenuContainer = document.getElementsByClassName('navigation')[0];
 
-let dataForTopMenu = Object.values(TOP_MENU).sort((a, b) => {
-    return a.order - b.order;
-});
-
-let topMenuFilling = '<ul>';
-let amountOfMenuItems = Math.min(9, dataForTopMenu.length);
-for (let i = 0; i < amountOfMenuItems; i++) {
-    let result = '<li>';
-    if (dataForTopMenu[i].submenu) {
-        result += `<p class="dropdownMenu">${dataForTopMenu[i].title}</p><div>`;
-        dataForTopMenu[i].submenu.map((subMenuItem) => {
-            result += `<a href="${subMenuItem.url}">${subMenuItem.title}</a>`;
+        let dataForTopMenu = Object.values(TOP_MENU).sort((a, b) => {
+            return a.order - b.order;
         });
-        result += '</div>'
-    } else {
-        result += `<a href="${dataForTopMenu[i].url}">${dataForTopMenu[i].title}</a>`
-    }
-    result += '</li>';
-    topMenuFilling += result;
-}
-topMenuFilling += '</ul>';
 
-topMenuContainer.innerHTML = topMenuFilling;
+        let topMenuFilling = '<ul>';
+        let amountOfMenuItems = Math.min(9, dataForTopMenu.length);
+        for (let i = 0; i < amountOfMenuItems; i++) {
+            let result = '<li>';
+            if (dataForTopMenu[i].submenu) {
+                result += `<p class="dropdownMenu">${dataForTopMenu[i].title}</p><div>`;
+                dataForTopMenu[i].submenu.map((subMenuItem) => {
+                    result += `<a href="${subMenuItem.url}">${subMenuItem.title}</a>`;
+                });
+                result += '</div>'
+            } else {
+                result += `<a href="${dataForTopMenu[i].url}">${dataForTopMenu[i].title}</a>`
+            }
+            result += '</li>';
+            topMenuFilling += result;
+        }
+        topMenuFilling += '</ul>';
+
+        topMenuContainer.innerHTML = topMenuFilling;
+    } else {
+        throw new Error('There is no data for top menu');
+    }
+} catch (e) {
+    console.error(e);
+}
+
 
 /*
  * Data for cart
  */
 
-let cart = document.getElementsByClassName('cartMain')[0].childNodes[3];
-cart.innerText = `${BASKET.elements} / ${BASKET.price}${CURRENCY === 'UAH' ? 'грн.' : 'р.'}`
+let cart = document.getElementsByClassName('cartMain')[0];
+if (BASKET.elements && BASKET.price) {
+    cart.childNodes[3].innerText = `${BASKET.elements} / ${BASKET.price}${CURRENCY === 'UAH' ? 'грн.' : 'р.'}`;
+} else {
+    cart.childNodes[1].innerText = 'КОРЗИНА ПУСТА'
+}
+
+/*
+ * Catalog menu realisation
+ */
+
+try {
+    if (MENU.length !== 0) {
+        let catalogMenuContainer = document.getElementsByClassName('catalogMenu')[0];
+
+        let dataForCatalogMenu = MENU.sort((a, b) => {
+            return a.order - b.order;
+        });
+
+        let catalogMenuFilling = '';
+
+        if (dataForCatalogMenu.length >= 1 && dataForCatalogMenu.length <= 10) {
+            catalogMenuFilling += '<ul>';
+            dataForCatalogMenu.map((item) => {
+                catalogMenuFilling += `<li class="catalogMenuItem">
+                <a href="${item.url}">
+                  ${item.title}
+                </a>
+              </li>`
+            });
+            catalogMenuFilling += '</ul>';
+            catalogMenuContainer.innerHTML = catalogMenuFilling;
+        } else {
+            let catalogMenuShowingData = dataForCatalogMenu.slice(0,10);
+            let lastItemIndex = 9;
+
+            updateCatalogMenu(catalogMenuShowingData);
+
+            function updateCatalogMenu (data) {
+                catalogMenuFilling = '';
+
+                if (lastItemIndex === 9) {
+                    catalogMenuFilling += '<ul>\n'
+                } else {
+                    catalogMenuFilling += '<ul>\n' +
+                        '          <li class="catalogMenuButton">\n' +
+                        '            <button onclick="catalogMenuLeftButtonHandler(event)">\n' +
+                        '              <img src="images/icons/catalogLeftButton.png" alt="Меню каталога влево">\n' +
+                        '            </button>\n' +
+                        '          </li>';
+                }
+
+                data.map((item) => {
+                    catalogMenuFilling += `<li class="catalogMenuItem">
+                <a href="${item.url}">
+                  ${item.title}
+                </a>
+              </li>`
+                });
+
+
+                if (lastItemIndex === dataForCatalogMenu.length-1) {
+                    catalogMenuFilling += '        </ul>';
+                } else {
+                    catalogMenuFilling += '<li class="catalogMenuButton">\n' +
+                        '            <button onclick="catalogMenuRightButtonHandler(event)">\n' +
+                        '              <img src="images/icons/catalogRightButton.png" alt="Меню каталога влево">\n' +
+                        '            </button>\n' +
+                        '          </li>\n' +
+                        '        </ul>';
+                }
+
+
+                catalogMenuContainer.innerHTML = catalogMenuFilling;
+
+            }
+
+            function catalogMenuLeftButtonHandler () {
+                lastItemIndex--;
+                catalogMenuShowingData = dataForCatalogMenu.slice(lastItemIndex-9, lastItemIndex+1);
+                updateCatalogMenu(catalogMenuShowingData);
+
+            }
+
+            function catalogMenuRightButtonHandler () {
+                lastItemIndex++;
+                catalogMenuShowingData = dataForCatalogMenu.slice(lastItemIndex-9, lastItemIndex+1);
+                updateCatalogMenu(catalogMenuShowingData);
+            }
+        }
+
+    } else {
+        throw new Error('There is no data for menu');
+    }
+} catch (e) {
+    console.error(e);
+}
+
+
